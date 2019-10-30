8c7b3ccef75cbc658cef0f830b956e65887c26be
Maulid <56664255+Maulid70@users.noreply.github.com>
2019-10-30 22:09:06 +0700

    Update 8._Pasangan_Angka_Terbesar.js

--- Fase 0/Week 3/rabu/8._Pasangan_Angka_Terbesar.js
+++ Fase 0/Week 3/rabu/8._Pasangan_Angka_Terbesar.js
@@ -14,11 +14,10 @@ function pasanganTerbesar(num) {
     }
     return temp;
 }
-  console.log(pasanganTerbesar(641573));
-//   `${str[i]}${str[i+1]}`
-  // TEST CASES
-//   console.log(pasanganTerbesar(641573)); // 73
-//   console.log(pasanganTerbesar(12783456)); // 83
-//   console.log(pasanganTerbesar(910233)); // 91
-//   console.log(pasanganTerbesar(71856421)); // 85
-//   console.log(pasanganTerbesar(79918293)); // 99
+
+//   TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
