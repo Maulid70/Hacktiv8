15f5eadd2dadb7f2dc1c7f7fae116ed329d79389
Maulid70 <maulid.ahmad@gmail.com>
2019-10-31 14:12:39 +0700

    nothing impossible

--- Fase 0/Week 3/jumat/14._Mengelempokkan_Angka.js
+++ Fase 0/Week 3/jumat/14._Mengelempokkan_Angka.js
@@ -1,9 +1,28 @@
 function mengelompokkanAngka(arr) {
     // you can only write your code here!
-  }
-  
-  // TEST CASES
-  console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
-  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
-  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
-  console.log(mengelompokkanAngka([])); // [ [], [], [] ]
+    var t1 = []
+    var t2 = []
+    var t3 = []
+    var tf = []
+    // console.log(beda);
+
+    for(i = 0; i <= arr.length; i++){
+        if(arr[i]%2 === 0 && arr[i]%3 !==0){
+            t1.push(arr[i])
+        }
+        else if(arr[i]%2 === 1&& arr[i]%3 !==0){
+            t2.push(arr[i])
+        }
+        else if(arr[i]%3 === 0){
+            t3.push(arr[i])
+        }
+    }
+    tf.push(t1,t2,t3)
+    return tf
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
